ee4eca61a434165826e92318fb287789b5fef984
molindu <molinduyasodha@gmail.com>
2025-06-17 17:32:46 +0530

    app changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css'
+import ItemsLoader from "./component/ItemsLoader.tsx";
+import CartDetails from "./component/CartDetails.tsx";
+import ConfirmModal from "./models/ConfirmModal.tsx";
+import useCommonStore from "./store/useCommonStore.ts";
 
 function App() {
-
+    const {modalOpen} = useCommonStore((state) => state)
     return (
         <>
+            <main>
+                <ItemsLoader/>
+                <CartDetails/>
+            </main>
+            {modalOpen &&
+                <ConfirmModal/>
+            }
         </>
     )
 }
